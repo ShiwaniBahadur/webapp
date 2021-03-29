3d492f64c21e90bceb40274ed982903c8b2ee70a
ShiwaniBahadur <51435918+ShiwaniBahadur@users.noreply.github.com>
2021-03-29 13:03:52 +0530

    Update db.js

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://webappdb:weappdbpassword@cluster0.q9sni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser: true}, {useUnifiedTopology: true}).then(()=>{
+mongoose.connect("mongodb+srv://webappdb:webappdbpassword@cluster0.q9sni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser: true}, {useUnifiedTopology: true}).then(()=>{
     console.log("Database connected successfully");
 }).catch((err)=>{
     console.log("Error in connecting to Database " + err);
