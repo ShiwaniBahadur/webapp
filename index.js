410eba3a0919f3a5313298dbb61be8676ae9496c
ShiwaniBahadur <shiwanibahadur@gmail.com>
2021-03-30 17:38:26 +0530

    commit

--- index.js
+++ index.js
@@ -12,11 +12,8 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
 app.use('/', api);
 
-=======
->>>>>>> 8a5f51931b1884501b46f6b49a322540667d7a23
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'),
   res.setHeader('Access-Control-Allow-Credentials' , true),
@@ -24,19 +21,12 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers' , 'Origin, Content-Type, Accept')
 })
 
-<<<<<<< HEAD
 
-app.use(express.static(__dirname + "/dist/webapp1/index.html"));
-=======
-app.use('/', api);
-
-app.use(express.static(__dirname+"/dist/webapp/index.html"));
-
->>>>>>> 8a5f51931b1884501b46f6b49a322540667d7a23
+app.use(express.static(__dirname+"/dist/webapp1"));
 
 
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/webapp/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/webapp1/index.html'));
 })
 
 
