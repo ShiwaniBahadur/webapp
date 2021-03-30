93d7207e42f73d56a5158252da2caac4c27e9e74
ShiwaniBahadur <shiwanibahadur@gmail.com>
2021-03-30 16:22:11 +0530

    update index.js

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ const path = require('path');
 
 var app = express();
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+const port = process.env.PORT || 3000;
 
 app.use('/', api);
 
 
 app.use(express.static(__dirname+"/dist/webapp/index.html"));
 
-app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/webapp/index.html'));
-})
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'),
@@ -27,9 +26,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods' , 'GET, POST, DELETE, PUT, OPTIONS'),
   res.setHeader('Access-Control-Allow-Headers' , 'Origin, Content-Type, Accept')
 })
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
 
-app.listen(port, host, ()=>{
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname + '/dist/webapp/index.html'));
+})
+
+
+
+app.listen(port, ()=>{
     console.log("Server is running at http://localhost:" + port);
 })
